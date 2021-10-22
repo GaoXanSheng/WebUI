b106e50fefb4107a718cac4706a3e31109c43364
GaoXuShenG <2425354040@qq.com>
2021-10-22 16:47:58 +0800

    optimize performance

--- src/main/java/www/yunmouren/util/Browser.java
+++ src/main/java/www/yunmouren/util/Browser.java
@@ -3,29 +3,21 @@ package www.yunmouren.util;//
 // (powered by FernFlower decompiler)
 //
 
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.renderer.GlStateManager;
 import net.montoyo.mcef.api.API;
 import net.montoyo.mcef.api.IBrowser;
 import net.montoyo.mcef.api.MCEFApi;
-import net.montoyo.mcef.example.ExampleMod;
-import net.montoyo.mcef.example.ScreenCfg;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 
 public class Browser extends GuiScreen {
     IBrowser browser = null;
-    private GuiButton back = null;
-    private GuiButton fwd = null;
-    private GuiButton go = null;
-    private GuiButton min = null;
-    private GuiButton vidMode = null;
     private GuiTextField url = null;
     private String urlToLoad;
 
-    public Browser() {
+    public Browser() { 
     }
 
     public Browser(String url) {
@@ -48,23 +40,9 @@ public class Browser extends GuiScreen {
         Keyboard.enableRepeatEvents(true);
         this.buttonList.clear();
         if (this.url == null) {
-            this.buttonList.add(this.back = new GuiButton(0, 0, 0, 0, 0, ""));
-            this.buttonList.add(this.fwd = new GuiButton(1, 0, 0, 0, 0, ""));
-            this.buttonList.add(this.go = new GuiButton(2, 0, 0, 0, 0, ""));
-            this.buttonList.add(this.min = new GuiButton(3, 0, 0, 0, 0, ""));
-            this.buttonList.add(this.vidMode = new GuiButton(4, 0, 0, 20, 0, ""));
-            this.vidMode.enabled = true;
             this.url = new GuiTextField(5, this.fontRenderer, 0, 0, 0, 0);
             this.url.setMaxStringLength(65535);
         } else {
-            this.buttonList.add(this.back);
-            this.buttonList.add(this.fwd);
-            this.buttonList.add(this.go);
-            this.buttonList.add(this.min);
-            this.buttonList.add(this.vidMode);
-            this.vidMode.x = this.width - 40;
-            this.go.x = this.width - 60;
-            this.min.x = this.width - 20;
             String old = this.url.getText();
             this.url = new GuiTextField(5, this.fontRenderer, 0, 0, 0, 0);
             this.url.setMaxStringLength(65535);
@@ -98,7 +76,7 @@ public class Browser extends GuiScreen {
             GlStateManager.disableDepth();
             GlStateManager.enableTexture2D();
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            this.browser.draw(0.0D, (double)this.height, (double)this.width, 0.0D);
+            this.browser.draw(0.0D, this.height, this.width, 0.0D);
             GlStateManager.enableDepth();
         }
 
@@ -110,31 +88,20 @@ public class Browser extends GuiScreen {
         int num;
         while (Keyboard.next()) {
             if (Keyboard.getEventKey() == 1) {
-                this.mc.displayGuiScreen((GuiScreen) null);
+                this.mc.displayGuiScreen(null);
                 return;
             }
 
-            pressed = Keyboard.getEventKeyState();
             pressed = this.url.isFocused();
             char key = Keyboard.getEventCharacter();
             num = Keyboard.getEventKey();
             if (this.browser != null && !pressed) {
-                if (pressed) {
-                    this.browser.injectKeyPressedByKeyCode(num, key, 0);
-                } else {
-                    this.browser.injectKeyReleasedByKeyCode(num, key, 0);
-                }
+                this.browser.injectKeyReleasedByKeyCode(num, key, 0);
 
                 int code = 0;
                 if (key != 0) {
                     this.browser.injectKeyTyped(key, code, 0);
                 }
-            }
-
-            if (!pressed && pressed && num == 28) {
-                this.actionPerformed(this.go);
-            } else if (pressed) {
-                this.url.textboxKeyTyped(key, num);
             }
         }
 
@@ -164,54 +131,7 @@ public class Browser extends GuiScreen {
                     this.browser.injectMouseButton(sx, y, 0, btn_, pressed, 1);
                 }
             }
-
-            if (pressed) {
-                y = sx * this.width / this.mc.displayWidth;
-                y = this.height - num * this.height / this.mc.displayHeight - 1;
-
-                try {
-                    this.mouseClicked(y, y, btn);
-                } catch (Throwable var9) {
-                    var9.printStackTrace();
-                }
-
-                this.url.mouseClicked(y, y, btn);
-            }
         }
 
-    }
-
-    protected void actionPerformed(GuiButton src) {
-        if (this.browser != null) {
-            if (src.id == 0) {
-                this.browser.goBack();
-            } else if (src.id == 1) {
-                this.browser.goForward();
-            } else {
-                String loc;
-                if (src.id == 2) {
-                    loc = ExampleMod.INSTANCE.getAPI().punycode(this.url.getText());
-                    this.browser.loadURL(loc);
-                } else if (src.id == 3) {
-                    this.mc.displayGuiScreen((GuiScreen) null);
-                } else if (src.id == 4) {
-                    loc = this.browser.getURL();
-                    String vId = null;
-                    boolean redo = false;
-                    if (loc.matches("^https?://(?:www\\.)?youtube\\.com/watch\\?v=([a-zA-Z0-9_\\-]+)$")) {
-                        vId = loc.replaceFirst("^https?://(?:www\\.)?youtube\\.com/watch\\?v=([a-zA-Z0-9_\\-]+)$", "$1");
-                    } else if (loc.matches("^https?://(?:www\\.)?youtu\\.be/([a-zA-Z0-9_\\-]+)$")) {
-                        vId = loc.replaceFirst("^https?://(?:www\\.)?youtu\\.be/([a-zA-Z0-9_\\-]+)$", "$1");
-                    } else {
-                        redo = true;
-                    }
-
-                    if (vId != null || redo) {
-                        this.mc.displayGuiScreen(new ScreenCfg(this.browser, vId));
-                    }
-                }
-            }
-
-        }
     }
 }
